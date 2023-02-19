47819ad18c844b3b3fff5bdebaddfae99fdd09ae
SeraphicJoy <112933642+SeraphicJoy@users.noreply.github.com>
2023-02-19 06:18:21 +0000

    Updated Gravity calculator

--- homeworks/PlanetGravityCalc/main.py
+++ homeworks/PlanetGravityCalc/main.py
@@ -1,8 +1,7 @@
 import math
 G=6.6743*10**(-11) #Gravitational Constant
-m1=float(input("Insert 1st planet mass (without power of 10):"))
-m1*=math.pow(10,int(input("Insert power of 10:")))
-m2=float(input("Insert 2nd planet mass (without power of 10):"))
+m1=5.97600*math.pow(10,24) #Mass of Earth
+m2=float(input("Insert 2nd planet mass (without power of 10):")) #Mass of 2nd planet in kg
 m2*=math.pow(10,int(input("Insert power of 10:")))
 R=float(input("Insert distance between planets (in km):"))*1000 #Converts kilometres to metres
 if m1>0 and m2>0 and R>0:
